2f5a129b47ae1309b4799feca3b4e73e79ca1e15
cyril-jump <k.v.agafonov@gmail.com>
2022-04-19 20:40:37 +0300

    Code increment #3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,16 +4,23 @@ import (
 	"github.com/cyril-jump/shortener/internal/app/handlers"
 	"github.com/cyril-jump/shortener/internal/app/storage"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-const servAdr = "localhost:8080"
-
 func main() {
-
+	//db
 	url := storage.NewURL()
 
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	//Routes
 	e.GET("/:id", handlers.GetURL(url))
 	e.POST("/", handlers.PostURL(url))
-	e.Logger.Info(e.Start(":8080"))
+
+	// Start Server
+	e.Logger.Fatal(e.Start(":8080"))
 }
--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -9,13 +9,23 @@ import (
 	"net/http"
 )
 
+// Const and Var
+const host = "http://localhost:8080/"
+
+var shortURL = ""
+
+// Handlers
+
 func PostURL(url *storage.URL) echo.HandlerFunc {
 	return func(c echo.Context) error {
+
 		body, err := io.ReadAll(c.Request().Body)
+
 		if err != nil || len(body) == 0 {
 			return c.NoContent(http.StatusBadRequest)
+		} else {
+			shortURL = hash(body)
 		}
-		shortURL := hash(body)
 
 		url.Short[shortURL] = string(body)
 
@@ -23,26 +33,27 @@ func PostURL(url *storage.URL) echo.HandlerFunc {
 	}
 }
 
-func GetURL(url *storage.URL) echo.HandlerFunc {
+func GetURL(db *storage.URL) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
 		if c.Param("id") == "" {
 			return c.NoContent(http.StatusBadRequest)
+		} else {
+			shortURL = host + c.Param("id")
 		}
 
-		shortURL := "http://localhost:8080/" + c.Param("id")
-
-		if url.Short[shortURL] == "" {
+		if db.Short[shortURL] == "" {
 			return c.NoContent(http.StatusBadRequest)
+		} else {
+			c.Response().Header().Set("Location", db.Short[shortURL])
+			return c.NoContent(http.StatusTemporaryRedirect)
 		}
 
-		c.Response().Header().Set("Location", url.Short[shortURL])
-		return c.NoContent(http.StatusTemporaryRedirect)
 	}
 }
 
+// other func
 func hash(url []byte) string {
-	host := "http://localhost:8080/"
 	hash := md5.Sum(url)
 	return fmt.Sprintf("%s%x", host, hash)
 }
